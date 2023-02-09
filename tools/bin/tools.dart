ef57f4bd43a4a3869a8dc4f24af96172cb586fb2
Matthew Keller <matthew.ed.keller@gmail.com>
2023-02-09 07:35:30 -0500

    remove tools directory

--- tools/bin/tools.dart
+++
@@ -1,5 +0,0 @@
-import 'package:tools/tools.dart' as tools;
-
-void main(List<String> arguments) {
-  print('Hello world: ${tools.calculate()}!');
-}
--- tools/test/tools_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:tools/tools.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
