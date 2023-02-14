e88c7d5fd26b18d919f569b0f449d72a069c1d22
Matthew Keller <matthew.ed.keller@gmail.com>
2023-02-14 08:31:32 -0500

    edit item names

--- goals_web/lib/goal_viewer/goal_item.dart
+++ goals_web/lib/goal_viewer/goal_item.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:goals_core/model.dart' show Goal;
+import 'package:goals_core/sync.dart';
 
-class GoalItemWidget extends StatelessWidget {
+import '../app_context.dart';
+import '../styles.dart';
+
+class GoalItemWidget extends StatefulWidget {
   final Goal goal;
   final bool selected;
   final Function(bool? value) onSelected;
@@ -12,11 +16,61 @@ class GoalItemWidget extends StatelessWidget {
       required this.onSelected});
 
   @override
+  State<GoalItemWidget> createState() => _GoalItemWidgetState();
+}
+
+class _GoalItemWidgetState extends State<GoalItemWidget> {
+  TextEditingController? _textController;
+  bool _editing = false;
+  FocusNode _focusNode = FocusNode();
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    if (_textController == null) {
+      _textController = TextEditingController(text: widget.goal.text);
+    } else {
+      _textController!.text = widget.goal.text;
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Checkbox(value: selected, onChanged: onSelected),
-        Text(goal.text),
+        Checkbox(value: widget.selected, onChanged: widget.onSelected),
+        _editing
+            ? SizedBox(
+                width: 200,
+                child: TextField(
+                  autocorrect: false,
+                  controller: _textController,
+                  decoration: null,
+                  style: mainTextStyle,
+                  onEditingComplete: () {
+                    AppContext.of(context).syncClient.modifyGoal(GoalDelta(
+                        id: widget.goal.id, text: _textController!.text));
+                    setState(() {
+                      _editing = false;
+                    });
+                  },
+                  onTapOutside: (_) {
+                    setState(() {
+                      _editing = false;
+                    });
+                  },
+                  focusNode: _focusNode,
+                ))
+            : GestureDetector(
+                onDoubleTap: () => {
+                  setState(() {
+                    _editing = true;
+                    _focusNode.requestFocus();
+                  })
+                },
+                child: Text(widget.goal.text, style: mainTextStyle),
+              ),
       ],
     );
   }
---
+++ goals_web/lib/styles.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart' show Colors;
+import 'package:flutter/widgets.dart' show TextStyle;
+
+const mainTextStyle = TextStyle(fontSize: 12, color: Colors.black);
