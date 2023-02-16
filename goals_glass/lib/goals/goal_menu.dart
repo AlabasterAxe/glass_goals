b8e91218c08e961a855d43ce4562ed2345379da4
Matthew Keller <matthew.ed.keller@gmail.com>
2023-02-16 09:04:00 -0500

    add active subgoal

--- goals_glass/lib/goals/goal_menu.dart
+++ goals_glass/lib/goals/goal_menu.dart
@@ -10,10 +10,13 @@ import 'package:flutter/widgets.dart'
         Widget;
 import 'package:goals_core/model.dart' show Goal, WorldContext, goalHasStatus;
 import 'package:goals_core/sync.dart';
+import 'package:uuid/uuid.dart';
 
 import '../styles.dart';
+import '../util/app_context.dart';
 import '../util/glass_gesture_detector.dart';
 import '../util/glass_page_view.dart';
+import 'add_subgoal_card.dart';
 
 class GoalMenu extends StatelessWidget {
   final void Function() onArchive;
@@ -30,51 +33,73 @@ class GoalMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<Widget> cards = [
+      GlassGestureDetector(
+        onTap: () {
+          onSetActive();
+          Navigator.pop(context);
+        },
+        child: Container(
+            color: Colors.black.withOpacity(0.5),
+            child: Center(
+                child: Text(
+                    goalHasStatus(
+                                WorldContext.now(), goal, GoalStatus.active) !=
+                            null
+                        ? 'Deactivate'
+                        : 'Activate',
+                    style: mainTextStyle))),
+      ),
+      GlassGestureDetector(
+        onTap: () {
+          onDone();
+          Navigator.pop(context);
+        },
+        child: Container(
+            color: Colors.black.withOpacity(0.5),
+            child: Center(
+                child: Text(
+                    goalHasStatus(WorldContext.now(), goal, GoalStatus.done) !=
+                            null
+                        ? 'Reopen'
+                        : 'Mark Complete',
+                    style: mainTextStyle))),
+      ),
+      GlassGestureDetector(
+        onTap: () {
+          onArchive();
+          Navigator.pop(context);
+        },
+        child: Container(
+            color: Colors.black.withOpacity(0.5),
+            child: const Center(child: Text('Archive', style: mainTextStyle))),
+      ),
+    ];
+
+    if (goalHasStatus(WorldContext.now(), goal, GoalStatus.active) != null) {
+      cards.add(AddSubGoalCard(
+        onGoalText: (text) {
+          AppContext.of(context).syncClient.modifyGoal(
+                GoalDelta(
+                  id: const Uuid().v4(),
+                  parentId: goal.id,
+                  text: text,
+                  statusLogEntry: StatusLogEntry(
+                    status: GoalStatus.active,
+                    creationTime: DateTime.now(),
+                    startTime: DateTime.now(),
+                    endTime: DateTime.now().add(const Duration(hours: 1)),
+                  ),
+                ),
+              );
+        },
+        onError: (e) {
+          Navigator.pop(context);
+        },
+      ));
+    }
     return GlassPageView(
-      children: [
-        GlassGestureDetector(
-          onTap: () {
-            onSetActive();
-            Navigator.pop(context);
-          },
-          child: Container(
-              color: Colors.black.withOpacity(0.5),
-              child: Center(
-                  child: Text(
-                      goalHasStatus(WorldContext.now(), goal,
-                                  GoalStatus.active) !=
-                              null
-                          ? 'Deactivate'
-                          : 'Activate',
-                      style: mainTextStyle))),
-        ),
-        GlassGestureDetector(
-          onTap: () {
-            onDone();
-            Navigator.pop(context);
-          },
-          child: Container(
-              color: Colors.black.withOpacity(0.5),
-              child: Center(
-                  child: Text(
-                      goalHasStatus(
-                                  WorldContext.now(), goal, GoalStatus.done) !=
-                              null
-                          ? 'Reopen'
-                          : 'Mark Complete',
-                      style: mainTextStyle))),
-        ),
-        GlassGestureDetector(
-          onTap: () {
-            onArchive();
-            Navigator.pop(context);
-          },
-          child: Container(
-              color: Colors.black.withOpacity(0.5),
-              child:
-                  const Center(child: Text('Archive', style: mainTextStyle))),
-        ),
-      ],
+      children: cards,
     );
   }
 }
