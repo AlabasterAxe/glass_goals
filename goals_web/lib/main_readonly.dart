4644c8ea4d42418354bd6105cf3c3c29a953328b
Matthew Keller <matthew.ed.keller@gmail.com>
2023-10-09 07:24:17 -0400

    register the provider

--- goals_web/lib/main_readonly.dart
+++ goals_web/lib/main_readonly.dart
@@ -1,4 +1,6 @@
 import 'package:firebase_core/firebase_core.dart' show Firebase;
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart';
 import 'package:flutter/material.dart';
 import 'package:goals_core/sync.dart' show FirestorePersistenceService;
 import 'package:goals_web/firebase_options.dart';
@@ -12,6 +14,12 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await Hive.initFlutter();
+
+  FirebaseUIAuth.configureProviders([
+    GoogleProvider(
+        clientId:
+            '114797465949-keupvd032s4to34t1bkftge1baoguld5.apps.googleusercontent.com'),
+  ]);
 
   usePathUrlStrategy();
   runApp(ProviderScope(
