c221a40998aa941184025c5146da5ae33bfe9125
Matthew Keller <matthew.ed.keller@gmail.com>
2023-10-17 07:13:20 -0400

    looks reasonable

--- goals_web/lib/goal_viewer/text_editing_controls.dart
+++ goals_web/lib/goal_viewer/text_editing_controls.dart
@@ -1,11 +1,32 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+
+import '../styles.dart';
 
 class TextEditingControls extends StatelessWidget {
   const TextEditingControls({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        SizedBox(
+          width: uiUnit(15),
+          child: IconButton(
+            icon: const Icon(Icons.close),
+            onPressed: () {},
+          ),
+        ),
+        SizedBox(
+          width: uiUnit(15),
+          child: IconButton(
+            icon: const Icon(Icons.check),
+            onPressed: () {},
+          ),
+        )
+      ],
+    );
   }
 }
