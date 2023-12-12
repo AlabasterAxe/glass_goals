e631ef450afcad130d83099de859ce3ba7b59935
Matthew Keller <matthew.ed.keller@gmail.com>
2023-12-12 10:56:48 -0500

    a couple more minor improvements

--- goals_web/lib/goal_viewer/goal_search_modal.dart
+++ goals_web/lib/goal_viewer/goal_search_modal.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:goals_core/model.dart' show Goal;
 
-import '../app_context.dart';
-
 class GoalSearchModal extends StatefulWidget {
   final Map<String, Goal> goalMap;
   const GoalSearchModal({super.key, required this.goalMap});
@@ -13,23 +11,34 @@ class GoalSearchModal extends StatefulWidget {
 
 class _GoalSearchModalState extends State<GoalSearchModal> {
   final _textController = TextEditingController();
+  final _focusNode = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _focusNode.requestFocus();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final results = this
-        .widget
-        .goalMap
-        .values
-        .where((goal) => goal.text
-            .toLowerCase()
-            .contains(_textController.text.toLowerCase()))
-        .toList();
+    final results = _textController.text.length == 0
+        ? []
+        : this
+            .widget
+            .goalMap
+            .values
+            .where((goal) => goal.text
+                .toLowerCase()
+                .contains(_textController.text.toLowerCase()))
+            .toList();
     return SingleChildScrollView(
       child: Column(
         children: [
           TextField(
-            controller: this._textController,
-          ),
+              focusNode: this._focusNode,
+              controller: this._textController,
+              onChanged: (_) => setState(() {})),
           for (final result in results) Text(result.text),
         ],
       ),
