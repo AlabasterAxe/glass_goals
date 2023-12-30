a99543cc303b64ebfc4310014097adc8977f614f
Matthew Keller <matthew.ed.keller@gmail.com>
2023-12-30 08:29:17 -0500

    simple mouse record replay

---
+++ demo/index.ts
@@ -0,0 +1,35 @@
+import { By, Builder, Origin } from "selenium-webdriver";
+import * as events from "./mouse-events.json";
+
+(async function test() {
+  const driver = await new Builder().forBrowser("chrome").build();
+  await driver.get(
+    "file:///C:/Users/matth/Documents/glass_goals/demo/replay.html"
+  );
+  const startTime = Date.now();
+  const period = 10000;
+  const framerate = 60;
+  let eventIndex = 0;
+  let interval: ReturnType<typeof setInterval>;
+
+  interval = setInterval(() => {
+    const time = (Date.now() - startTime) % period;
+
+    let currentEvent = events[eventIndex];
+    while (currentEvent.time < time) {
+      const nextEvent = events[++eventIndex];
+      if (!nextEvent) {
+        clearInterval(interval);
+        break;
+      }
+      currentEvent = nextEvent;
+    }
+    driver
+      .actions()
+      .move({ x: currentEvent.x, y: currentEvent.y, duration: 1 })
+      .perform()
+      .catch(() => {
+        // ignore
+      });
+  }, 1000 / framerate);
+})();
---
+++ goals_web/integration_test/app_test.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:goals_core/sync.dart' show InMemoryPersistenceService;
+import 'package:goals_web/app.dart' show WebGoals;
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:integration_test/integration_test.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('end-to-end test', () {
+    testWidgets('tap on the floating action button, verify counter',
+        (tester) async {
+      // Load app widget.
+      await tester.pumpWidget(ProviderScope(
+          child: WebGoals(
+        shouldAuthenticate: false,
+        persistenceService: InMemoryPersistenceService(),
+      )));
+
+      await tester.pumpAndSettle();
+
+      // Verify the counter starts at 0.
+      expect(find.text('Scheduled Goals'), findsWidgets);
+    });
+  });
+}
